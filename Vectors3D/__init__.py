8306967b3e3058df3269f5b130cb59393478340c
TheoVasile <theo.vasile50@yahoo.com>
2020-07-12 16:15:49 -0400

    created cube and camera

--- Vectors3D/__init__.py
+++ Vectors3D/__init__.py
@@ -1,19 +1,24 @@
+#a point
 class Vertice:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+#an edge connecting two points
 class Edge:
     def __init__(self, vert1, vert2):
         self.vert1 = vert1
         self.vert2 = vert2
+#a face with a combination of points and edges
 class Face:
     def __init__(self, vertices):
         self.vertices = vertices
 
+#superclass for all objects
 class Object:
     def __init__(self, position, rotation, scale):
         self.positon = position
+        self.pivotPoint = position
         self.rotation = rotation
         self.scale = scale
     def get_pos(self):
@@ -29,6 +34,7 @@ class Object:
     def set_scale(self, scale):
         self.scale = scale
 
+#mesh is an object that contains vertices, edges, and faces
 class Mesh(Object):
     def __init__(self, position, rotation, scale, vertices, edges, faces):
         super().__init__(position, rotation, scale)
@@ -36,6 +42,7 @@ class Mesh(Object):
         self.edges = edges
         self.faces = faces
 
+#the camera projects the scene onto the screen
 class Camera(Object):
     def __init__(self, position, rotation, scale, fov):
         super().__init__(position, rotation, scale)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import Vectors3D as v3d
 
 pg.init()
 
@@ -8,6 +9,15 @@ screen = pg.display.set_mode((w, h))
 pg.display.set_caption(("v3d"))
 clock = pg.time.Clock()
 fps = 60
+
+#create a cube
+verts = [v3d.Vertice(-1, -1, 1), v3d.Vertice(-1, 1, 1), v3d.Vertice(1, 1, 1), v3d.Vertice(1, -1, 1), v3d.Vertice(1, -1, -1), v3d.Vertice(-1, -1, -1), v3d.Vertice(1, 1, -1), v3d.Vertice(-1, 1, -1)]
+edges = [v3d.Edge(verts[2], verts[3]), v3d.Edge(verts[2], verts[-2]), v3d.Edge(verts[2], verts[1]), v3d.Edge(verts[1], verts[-1]), v3d.Edge(verts[-2], verts[-1]), v3d.Edge(verts[-2], verts[4]), v3d.Edge(verts[4], verts[3]), v3d.Edge(verts[4], verts[5]), v3d.Edge(verts[3], verts[0]), v3d.Edge(verts[0], verts[1]), v3d.Edge(verts[0], verts[5]), v3d.Edge(verts[5], verts[-1])]
+faces = [v3d.Face([verts[0], verts[1], verts[-1], verts[5]]), v3d.Face([verts[1], verts[-1], verts[6], verts[2]]), v3d.Face([verts[2], verts[3], verts[0], verts[1]]), v3d.Face([verts[0], verts[3], verts[4], verts[5]]), v3d.Face([verts[3], verts[2], verts[6], verts[4]]), v3d.Face([verts[5], verts[-1], verts[6], verts[4]])]
+cube = v3d.Mesh([0, 0, 0], [0, 0, 0], [1, 1, 1], verts, edges, faces)
+
+#create a camera
+camera = v3d.Camera([-50, 0, 0], [0, 0, 0], [1, 1, 1], 30)
 
 running = True
 while running:
